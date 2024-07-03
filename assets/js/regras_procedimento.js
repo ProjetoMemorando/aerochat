810a8cf8d105833486a5902122d8f327124d9c18
ProjetoMemorando <77948017+ProjetoMemorando@users.noreply.github.com>
2024-07-03 17:29:26 -0300

    Update regras_procedimento.js

--- assets/js/regras_procedimento.js
+++ assets/js/regras_procedimento.js
@@ -82,7 +82,6 @@ Flexível (tecidos impermeáveis): até 24 cm de altura, 32 cm de largura e 43 c
  	✈︎Número da Agência:   
  	✈︎Número da Conta com Dígito:   
  	✈︎Tipo de Conta:   
- 
 ✈︎Bancos recomendados: Banco do Brasil; Banrisul; Bradesco; HSBC; Itaú; Santander. O prazo para o estorno do valor varia de acordo com a forma de pagamento utilizada na compra. Cada método de pagamento possui um tempo de processamento diferente, o que pode influenciar no tempo necessário para que o valor seja devolvido à sua conta.
 
  	-Dinheiro ou débito: A Gol tem até 7 dias corridos para reembolsar.
@@ -91,23 +90,62 @@ Flexível (tecidos impermeáveis): até 24 cm de altura, 32 cm de largura e 43 c
     ],
 
     regra_tarifa: [
-        `Regra da tarifa (Em desenvolvimento)`
+`Tarifa Promo:
+✈︎Bagagem despachada: Na PROMO não tem direito a bagagem gratuita despachada;
+✈︎Marcação de assento: Custo adicional, exceto durante o check-in, onde é gratuita (de 48 horas a 1 hora antes do voo).
+✈︎Remarcação (Mudança de data): Você paga 100% do valor da passagem para remarcar, pagando o valor total da nova tarifa escolhida.
+✈︎Cancelamento: Em caso de cancelamento, o valor da passagem não será reembolsado.
+✈︎Reembolso: Esta tarifa não permite reembolso, exceto para as taxas de embarque.
+✈︎Antecipação de voo: Esta tarifa não permite antecipação.
+✈︎No Show (Não comparecimento): Se você não comparecer ao embarque, será cobrado o valor total da passagem, que corresponde a 100% do seu preço original. Caso opte por uma nova tarifa, você pagará o valor total dessa tarifa.
+`
+,
+`Tarifa Light:
+✈︎Bagagem despachada: Na LIGHT não tem direito a bagagem gratuita despachada;
+✈︎Marcação de assento: Custo adicional, exceto durante o check-in, onde é gratuita (de 48 horas a 1 hora antes do voo).
+✈︎Remarcação (Mudança de data): Para postergar a data do voo, você pagará o valor de R$ 400,00 ou 100% do preço original da passagem, prevalecendo o menor entre os dois, se a nova passagem tiver um valor superior, será necessário cobrir a diferença de preço.
+✈︎Antecipação de voo: Você pode antecipar seu voo pagando uma taxa de R$ 100,00. Essa opção está disponível até 6 horas antes do horário do seu voo original.
+✈︎Cancelamento: Para cancelar a passagem, será cobrado o valor de R$ 400,00 ou 100% do preço da passagem, prevalecendo o menor entre os dois.
+✈︎Reembolso: Esta tarifa não permite reembolso, exceto pelas taxas de embarque.
+✈︎No Show (Não comparecimento): Em caso de não comparecimento ao voo, será cobrado R$ 500,00 ou 100% do valor da passagem, prevalecendo o menor entre os dois, além da diferença de preço caso haja disponibilidade de outra passagem similar.
+
+`
+,
+`Tarifa Plus:
+✈︎Bagagem despachada: Na PLUS você tem direito a primeira bagagem gratuita despachada;
+✈︎Marcação de assento: Você tem direito à marcação de assento gratuita, permitindo que escolha com antecedência onde deseja sentar-se durante o voo, sem custos adicionais.
+✈︎Remarcação (Mudança de data): Para postergar, você pagará R$ 370,00 ou 100% do valor da passagem, o que for mais barato. Se a nova passagem for mais cara, também será necessário pagar a diferença de preço.
+✈︎Antecipação de voo: Você pode antecipar seu voo de forma gratuita. Essa opção está disponível até 6 horas antes do horário do seu voo original.
+✈︎Cancelamento: Para cancelar sua passagem, será cobrada uma taxa de R$ 370,00 ou 100% do valor da passagem, prevalecendo o menor valor entre os dois.
+✈︎Reembolso: Após o cancelamento, você receberá de volta 40% do valor restante da passagem.
+✈︎No Show (Não comparecimento): Se você não comparecer ao voo, será cobrado R$ 470,00 ou 100% do valor da passagem, prevalecendo o menor valor entre os dois. Caso haja disponibilidade de outra passagem similar, também será necessário cobrir a diferença de preço.
+`
+,
+ `Tarifa Max: 
+ ✈︎Bagagem despachada: Na Max você tem direito a primeira e a segunda bagagem gratuita despachada.
+✈︎Marcação de assento: Você tem direito à marcação de assento gratuita, permitindo que escolha com antecedência onde deseja sentar-se durante o voo, sem custos adicionais.
+✈︎Assento GOL + Conforto: Você tem direito à marcação de assento Gol + Conforto, localizados nas fileiras de 1 a 5 em voos domésticos e nas saídas de emergência em voos nacionais e internacionais. Além de mais espaço, você terá prioridade no check-in com despacho de bagagem, embarque antecipado e compartimento de bagagem de mão exclusivo. 
+✈︎Remarcação (Mudança de data): Você tem a flexibilidade de remarcar sem custos adicionais de multa, disponível gratuitamente para qualquer horário no mesmo dia do voo original. No entanto, se a nova passagem tiver um valor superior, será necessário pagar a diferença de preço entre os bilhetes.
+`
     ],
     
     medif_fremec: [
-       `O passageiro precisa encaminhar ao seu médico pessoal o formulário de informação médica (MEDIF) para que ele possa preencher e assinar, sendo necessário enviar para à GOL entre 15 dias e 72 horas antes do embarque. Após análise, nossa equipe emitirá um parecer sobre a aptidão do passageiro para viajar, onde esse documento que comprovará a condição especial da pessoa. 
+    `O passageiro precisa encaminhar ao seu médico pessoal o formulário de informação médica (MEDIF) para que ele possa preencher e assinar, sendo necessário enviar para à GOL entre 15 dias e 72 horas antes do embarque. Após análise, nossa equipe emitirá um parecer sobre a aptidão do passageiro para viajar, onde esse documento que comprovará a condição especial da pessoa. 
 ✈︎Preenchimento do MEDIF:
 O formulário MEDIF deve ser assinado pelo médico pessoal do passageiro (apenas profissionais médicos com CRM válido são aceitos; assinaturas de psicólogos, fisioterapeutas e outros profissionais não serão aceitas).
 Todas as seções devem ser completamente preenchidas para melhor análise.
 Anexos:
     ✈︎*Anexo A:* Preenchido pelo passageiro ou seu responsável legal;
-✈︎Anexo B (Partes 1 e 2): Preenchido pelo médico do passageiro com necessidades especiais.
+    ✈︎Anexo B (Partes 1 e 2): Preenchido pelo médico do passageiro com necessidades especiais.
 Todas as perguntas devem ser respondidas corretamente e com letra legível. Deve ser marcado (X) nas opções "Sim" ou "Não". Após o preenchimento, todas as vias do formulário devem ser assinadas e carimbadas pelo médico, o documento não pode conter danos.
-✈︎Importante: Os formulários devem estar completamente preenchidos e com informações atualizadas. Nenhum campo deve ficar em branco. Envie os documentos em anexo no formato JPEG ou PDF; imagens no corpo da mensagem não serão avaliadas.
+
+Importante: Os formulários devem estar completamente preenchidos e com informações atualizadas. Nenhum campo deve ficar em branco. Envie os documentos em anexo no formato JPEG ou PDF; imagens no corpo da mensagem não serão avaliadas.
 Documentos assinados digitalmente pelo responsável médico do passageiro serão aceitos, desde que validados através da plataforma https://validar.iti.gov.br/. A validação é gratuita e essencial para o aceite da documentação.
 
 ✈︎Viagem com acompanhante: Se necessário, a GOL oferece ao acompanhante um desconto de 80% na passagem aérea. O acompanhante deve ser maior de 18 anos e capaz de oferecer assistência ao passageiro com necessidade especial durante todo o voo. O desconto será aplicado somente após o preenchimento e aprovação dos formulários MEDIF.
-`,
+`
+,
+        
 `O Cartão FREMEC é um documento médico da IATA desenvolvido para simplificar viagens de clientes regulares com necessidades especiais de caráter permanente, não grave e estável.
 O cadastro FREMEC é indicado para Clientes com:
     ✈︎Idade superior a 2 (dois) anos;
@@ -155,14 +193,17 @@ Detalhes da bagagem de mão:
  	Peso permitido: até 10 kg;
  	Dimensões permitidas: 55 cm de altura, 25 cm de largura e 35 cm de comprimento.
 Essas medidas garantem que sua bagagem de mão se encaixe perfeitamente nos compartimentos da aeronave, proporcionando uma viagem mais confortável para todos
-`,
- 	`Você pode comprar a opção de bagagem despachada tanto no momento da compra da passagem quanto posteriormente. É importante seguir as especificações de tamanho para o despacho. Sua mala deve ter, no máximo, 50 cm de largura, 80 cm de altura e 28 cm de profundidade, com um peso de até 23 kg. Se adquirir a bagagem até 48hrs antes do voo (antes da janela de check-in) terá desconto na franquia.
+`
+,
+ `Você pode comprar a opção de bagagem despachada tanto no momento da compra da passagem quanto posteriormente. É importante seguir as especificações de tamanho para o despacho. Sua mala deve ter, no máximo, 50 cm de largura, 80 cm de altura e 28 cm de profundidade, com um peso de até 23 kg. Se adquirir a bagagem até 48hrs antes do voo (antes da janela de check-in) terá desconto na franquia.
 
- 	Se a bagagem ultrapassar 23 kg, será cobrado um valor por excesso de bagagem: R$ 35 por quilo para voos nacionais e R$ 50 por quilo para voos internacionais. O peso total de uma única bagagem não pode ultrapassar 45 kg para voos dentro do Brasil e 32 kg para voos internacionais. 
+ Se a bagagem ultrapassar 23 kg, será cobrado um valor por excesso de bagagem: R$ 35 por quilo para voos nacionais e R$ 50 por quilo para voos internacionais. O peso total de uma única bagagem não pode ultrapassar 45 kg para voos dentro do Brasil e 32 kg para voos internacionais. 
 
- 	Cada bagagem despachada deve corresponde a um único volume de até 23 kg. Se esse peso for dividido em dois ou mais volumes, cada volume extra será cobrado como bagagem adicional.`
-    ,
-        `A bagagem diferenciada compreende itens frágeis e com dimensão diferenciada, como equipamentos esportivos, instrumentos musicais, televisores, monitores e semelhantes. Nestes casos, cobra-se uma taxa específica para cada tipo de objeto pelo seu despacho.
+ Cada bagagem despachada deve corresponde a um único volume de até 23 kg. Se esse peso for dividido em dois ou mais volumes, cada volume extra será cobrado como bagagem adicional.
+   ` 
+,
+        
+`A bagagem diferenciada compreende itens frágeis e com dimensão diferenciada, como equipamentos esportivos, instrumentos musicais, televisores, monitores e semelhantes. Nestes casos, cobra-se uma taxa específica para cada tipo de objeto pelo seu despacho.
 ✈︎Como classificar como bagagem diferenciada:
  	-A soma dos lados A+B+C não pode ultrapassar 292 cm.
  	-A bagagem pode ter até 292 cm, levando em consideração a soma dos lados.
@@ -173,10 +214,7 @@ Essas medidas garantem que sua bagagem de mão se encaixe perfeitamente nos comp
 Os itens que são considerados como bagagem diferenciada devem pesar até 23 kg. Se excederem esse peso, será aplicada uma taxa por quilo excedente: R$ 35,00 por quilo para voos nacionais e R$ 50,00 por quilo para voos internacionais. No caso de viagens dentro do Brasil, o limite máximo permitido é de 45 kg, enquanto para viagens internacionais é de 32 kg. Caso o cliente tenha um volume que exceda essas condições, o despacho pode ser realizado pela GOLLOG, sendo necessário à validação do objeto transportado. 
 `
     ],
-    declaracao_valores: [
-            `em construção`
-            
-        ],
+
     
 }
 

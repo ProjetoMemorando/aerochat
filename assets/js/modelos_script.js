cf3dfcfd225611c32f7f9fcb5ec50758546cfd86
ProjetoMemorando <77948017+ProjetoMemorando@users.noreply.github.com>
2024-07-09 11:12:37 -0300

    Update modelos_script.js

--- assets/js/modelos_script.js
+++ assets/js/modelos_script.js
@@ -32,7 +32,7 @@ let modelos_scripts = {
 
         'Valorizamos a sua opinião sobre o atendimento GOL. Vou transferir para nossa pesquisa, sua avaliação é fundamental para nós. 💬✈️',
 
-        'A sua avaliação sobre o atendimento GOL é importante para nós. Por favor, deixe-nos saber como foi sua experência conosco através da nossa pesquisa. 💖✈️'
+        'A sua avaliação sobre o atendimento GOL é importante para nós. Por favor, deixe-nos saber como foi sua experiência conosco através da nossa pesquisa. 💖✈️'
     ],
 
     ausencia_do_cliente: [

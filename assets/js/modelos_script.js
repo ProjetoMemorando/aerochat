3e76bd7114cc19a6270b1192050bf21122db997f
ProjetoMemorando <77948017+ProjetoMemorando@users.noreply.github.com>
2024-07-15 09:59:40 -0300

    Update modelos_script.js

--- assets/js/modelos_script.js
+++ assets/js/modelos_script.js
@@ -98,7 +98,7 @@ function titulos(value) {
 
     // LISTA DE TITULOS PARA CADA SCRIPT (SE HOUVER)
     let lista_titulos = {
-        acordo_de_espera: '⚠ Em hipótese alguma você pode ficar mais de 2 minutos sem enviar mensagem para o cliente. Utilize o script abaixo para renovar o acordo de espera. ⚠',
+        acordo_de_espera: '⚠ Devemos utilizar os scripts abaixo para renovar o acordo de espera. É crucial sempre iniciar pelo primeiro script. ⚠',
 
         aviso_falta_de_interacao: '⚠ Caso perceba que o Cliente não responde, chame-o por 3 vezes (dando um intervalo de 02 minuto a cada chamada). Se ele não responder, encerre o contato. Sugestões de Script abaixo: ⚠'
 

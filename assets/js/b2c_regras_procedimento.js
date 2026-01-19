f534a924849cf1c3424d5d2f1f229c39ef7df63e
ProjetoMemorando <77948017+ProjetoMemorando@users.noreply.github.com>
2026-01-19 18:58:01 -0300

    Update b2c_regras_procedimento.js

--- assets/js/b2c_regras_procedimento.js
+++ assets/js/b2c_regras_procedimento.js
@@ -406,12 +406,15 @@ Nesse momento, o titular do cartão utilizado na compra deverá estar presente, 
     ] 
         ,
     
-    agencia: [ `XXX ,no atendimento da GOL, alguns procedimentos relacionados a reservas emitidas por agências, como alterações, cancelamentos ou correção de nome, são tratados diretamente pela própria agência responsável pela emissão.
+    agencia: [ ` XXXX, no atendimento da GOL, alguns procedimentos relacionados a reservas emitidas por agências, como alterações, cancelamentos ou correção de nome, são tratados diretamente pela própria agência responsável pela emissão.
 Da mesma forma, informações sobre valores de remarcação ou reembolso ficam a cargo da agência, pois ela realiza a intermediação entre o cliente e a GOL e pode ter regras específicas conforme o contrato firmado no momento da compra.
 Para dar continuidade à sua solicitação, oriento que entre em contato com a agência onde a compra foi realizada. Caso necessário, a agência poderá acionar os canais da GOL destinados ao atendimento de reservas de agência.
     `
+              
+    ]
         ,
-    `XXXX, entendi sua solicitação. Por este canal, atendemos reservas emitidas diretamente pela GOL, não sendo possível realizar procedimentos em reservas feitas por agências de viagem.
+    
+    agente: [ ` XXXX, entendi sua solicitação. Por este canal, atendemos reservas emitidas diretamente pela GOL, não sendo possível realizar procedimentos em reservas feitas por agências de viagem.
  
 Para dar continuidade, pedimos que faça o login no site utilizando os dados de acesso da agência. Assim, o atendimento será direcionado ao setor B2B, responsável pelas reservas emitidas por agências de viagem.
     `
@@ -526,6 +529,7 @@ elementoPai.addEventListener('click', (event) => {
         border_success(id_element);
     }
 });
+
 
 
 
